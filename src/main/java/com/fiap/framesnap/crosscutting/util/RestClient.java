73f80deff1a2bbe7522859ad29bdff73c9cea771
Marcelo Ebert <ebert.marcelo@gmail.com>
2025-04-04 17:46:20 -0300

    Add new Unit Tests

--- src/main/java/com/fiap/framesnap/crosscutting/util/RestClient.java
+++ src/main/java/com/fiap/framesnap/crosscutting/util/RestClient.java
@@ -16,37 +16,41 @@ public class RestClient {
     }
 
     public <T> ResponseEntity<T> post(String url, Object request, Class<T> responseType, Map<String, String> headers) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        headers.forEach(httpHeaders::set);
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
+        HttpHeaders httpHeaders = buildHeaders(headers, true);
         HttpEntity<Object> entity = new HttpEntity<>(request, httpHeaders);
         return restTemplate.exchange(url, HttpMethod.POST, entity, responseType);
     }
 
     public <T> ResponseEntity<T> get(String url, Class<T> responseType, Map<String, String> headers) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        headers.forEach(httpHeaders::set);
-
+        HttpHeaders httpHeaders = buildHeaders(headers, false);
         HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
         return restTemplate.exchange(url, HttpMethod.GET, entity, responseType);
     }
 
     public <T> ResponseEntity<T> put(String url, Object request, Class<T> responseType, Map<String, String> headers) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        headers.forEach(httpHeaders::set);
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
+        HttpHeaders httpHeaders = buildHeaders(headers, true);
         HttpEntity<Object> entity = new HttpEntity<>(request, httpHeaders);
         return restTemplate.exchange(url, HttpMethod.PUT, entity, responseType);
     }
 
     public ResponseEntity<Void> delete(String url, Map<String, String> headers) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        headers.forEach(httpHeaders::set);
-
+        HttpHeaders httpHeaders = buildHeaders(headers, false);
         HttpEntity<Void> entity = new HttpEntity<>(httpHeaders);
         return restTemplate.exchange(url, HttpMethod.DELETE, entity, Void.class);
     }
-}
 
+    private HttpHeaders buildHeaders(Map<String, String> headers, boolean ensureJsonContentType) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if (headers != null) {
+            headers.forEach(httpHeaders::set);
+        }
+
+        // Garante Content-Type: application/json apenas se não foi definido e for necessário
+        if (ensureJsonContentType && !httpHeaders.containsKey(HttpHeaders.CONTENT_TYPE)) {
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        }
+
+        return httpHeaders;
+    }
+}
